d829f373c6ce1aba5cdab93d0b1a39a8cce3f928
Makoto Ikami <makoto.ikami@gmail.com>
2022-12-02 15:57:32 +0900

    Personクラス周りの軽微な修正

--- src/Entity/Person.php
+++ src/Entity/Person.php
@@ -9,23 +9,27 @@ class Person
     private ?string $name = null;
     private ?string $mail = null;
 
-    public function getName(): string
+    public function getName(): ?string
     {
         return $this->name;
     }
 
-    public function setName(string $name): void
+    public function setName(string $name): static
     {
         $this->name = $name;
+
+        return $this;
     }
 
-    public function getMail(): string
+    public function getMail(): ?string
     {
         return $this->mail;
     }
 
-    public function setMail(string $mail): void
+    public function setMail(string $mail): static
     {
         $this->mail = $mail;
+
+        return $this;
     }
 }
